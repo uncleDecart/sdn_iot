fd1a956d90916235bd68b891d348c6dd64e19be7
Pavel Abramov <uncle.decart@gmail.com>
2020-05-06 23:53:34 +0300

    Added CORS support for 5556 route

--- network_handler.py
+++ network_handler.py
@@ -68,14 +68,34 @@ c1.cmd(ryu_cmd)
 net.start()
 net.pingAll()
 
-c1.start()
+#c1.start()
 
 for sn in switch_list:
   s = net[sn]
   s.start([c1])
 
+class EnableCors(object):
+  name = 'enable_cors'
+  api = 2
+
+  def apply(self, fn, context):
+    def _enable_cors(*args, **kwargs):
+      # set CORS headers
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+      if bottle.request.method != 'OPTIONS':
+        # actual request; reply with the actual response
+        return fn(*args, **kwargs)
+
+    return _enable_cors
+
 root = Dispatcher(net, switch_list)
 bottle.debug(True)
+bottle.install(EnableCors())
 bottle.run(app=root, host='0.0.0.0', port=5556)
 
 net.stop()
+
+

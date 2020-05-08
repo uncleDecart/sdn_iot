19e10ffed417ad0419815e30f8ddc44926d32894
Pavel Abramov <uncle.decart@gmail.com>
2020-05-08 21:43:50 +0300

    Added delete in CORS

--- network_handler.py
+++ network_handler.py
@@ -84,7 +84,7 @@ class EnableCors(object):
     def _enable_cors(*args, **kwargs):
       # set CORS headers
       response.headers['Access-Control-Allow-Origin'] = '*'
-      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE, PUT, OPTIONS'
       response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
       if bottle.request.method != 'OPTIONS':
